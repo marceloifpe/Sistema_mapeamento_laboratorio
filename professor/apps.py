486efadee297f887d13cb61adf47e796d50ebf29
Cleyson Silva de Oliveira <cleysonsilva2002@gmail.com>
2023-11-21 15:48:38 -0300

    Dashboard para o professor criada

--- professor/apps.py
+++ professor/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class GestorConfig(AppConfig):
+class ProfessorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestor'
+    name = 'professor'
