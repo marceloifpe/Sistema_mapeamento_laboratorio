a1de544113985a9ac073955ce6870b47ea078308
henriquesilva0035 <109631389+henriquesilva0035@users.noreply.github.com>
2023-11-24 13:18:20 -0300

    Criando APP salas

--- salas/models.py
+++ salas/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Salas(models.Model):
+
+    UABJ = 'UABJ'
+    AEB = 'AEB'
+
+    LOCAL_CHOICES = [
+        (UABJ, 'UABJ'),
+        (AEB, 'AEB'),
+    ]
+    nome_da_sala = models.CharField(max_length = 30)
+    local = models.CharField(max_length = 4, choices = LOCAL_CHOICES, default = UABJ)
+    reservado = models.BooleanField(default = False)
+    quem_reservou = models.CharField(max_length = 30)
+    data_reserva = models.DateTimeField()
+    data_devolucao = models.DateTimeField()
+    data_solicitacao = models.DateField(default = date.today)
+    
+    class Meta:
+        verbose_name = 'Sala'
+
+    def __str__(self):
+        return self.nome_da_sala
