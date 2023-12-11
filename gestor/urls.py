f1cbc9ce1b46755e106fea9b7f898df7e14ae47e
Marcelo Augusto <marcelo513.ma@gmail.com>
2023-12-11 11:36:29 -0300

    criando_url_gestor_ver_mat

--- gestor/urls.py
+++ gestor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('gestor_ver_salas/', views.gestor_ver_salas, name='gestor_ver_salas')
+    path('gestor_ver_salas/', views.gestor_ver_salas, name='gestor_ver_salas'),
+    path('gestor_ver_materiais/', views.gestor_ver_materiais, name='gestor_ver_materiais')
 ]   
